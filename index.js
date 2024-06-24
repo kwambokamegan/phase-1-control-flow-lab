abdb62f99ab10a9f29fe0727de4f96d79258458a
Megan <megan.nyakina@student.moringaschool.com>
2024-06-24 16:44:43 +0300

    adcs control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance){
+
+  if (distance>2500)
+    {return 'No can do.'}
+
+  else if(distance < 400)
+    {return "This one is on me!";}
+
+    else if(distance>=400 &&  distance <= 2000)
+      {return "That will be twenty bucks."}
+
+    else if(distance>2000)
+      {return "I will gladly take your thirty bucks."}
+
+    else if(distance>2500) 
+      {return "No can do."}
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(destination){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  return destination === 'NYC'? 'Ok, sounds good.' : 'No go.';
+  
+ }
+
+function switchOnCharmFromTip(tip = 0){
+
+if (typeof tip!== 'number') 
+  {return 'Bye' ;}
+
+if (tip >= 20) 
+  {return 'Thank you so much.';}
+
+else if (tip>=10)
+  {return 'Thank you.';}
+
+else{return 'Bye.';}
+
+
 }
