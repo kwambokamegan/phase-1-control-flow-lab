1e46077b8ae16fdb2af8d1386ae7284c2cc24f67
Megan <megan.nyakina@student.moringaschool.com>
2024-07-10 23:19:01 +0300

    adds control flow lab

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-
-function scuberGreetingForFeet(distance){
-
-  if (distance>2500)
-    {return 'No can do.'}
-
-  else if(distance < 400)
-    {return "This one is on me!";}
-
-    else if(distance>=400 &&  distance <= 2000)
-      {return "That will be twenty bucks."}
-
-    else if(distance>2000)
-      {return "I will gladly take your thirty bucks."}
-
-    else if(distance>2500) 
-      {return "No can do."}
-
-
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!';
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distanceInFeet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(destination){
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
 
-  return destination === 'NYC'? 'Ok, sounds good.' : 'No go.';
-  
- }
-
-function switchOnCharmFromTip(tip = 0){
-
-if (typeof tip!== 'number') 
-  {return 'Bye' ;}
-
-if (tip >= 20) 
-  {return 'Thank you so much.';}
-
-else if (tip>=10)
-  {return 'Thank you.';}
-
-else{return 'Bye.';}
-
-
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
